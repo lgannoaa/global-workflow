d52bb02a629afa020568874f8c35bcd16fe81de0
Lin.Gan <lin.gan@noaa.gov>
2021-04-19 16:09:42 +0000

    Created ecflow def files for C192C96L127 Modified ecflow jobs to remove WAVE, verification and downstream jobs. Modified ecflow scripts to use resource from the base line. Remove git ignore entry for the config.base file.

--- ecflow/ecf/include/model_ver.h
+++ ecflow/ecf/include/model_ver.h
@@ -2,7 +2,7 @@
 #### eval export HOME${model}=${NWROOT}/${model}.\${${model}_ver:?}
 
 . /gpfs/dell2/emc/modeling/noscrub/Lin.Gan/ecflow-dell/gfs.v16.0.0/ecflow/ecf/versions/${model:?}.ver
-export HOMEgfs=/gpfs/dell2/emc/modeling/noscrub/Lin.Gan/ecflow-dell/gfs.v16.0.0
-export HOMEobsproc_global=/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/gfsv16b/obsproc_global.iss71402.supportGFSv16
-export HOMEobsproc_network=/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/gfsv16b/obsproc_global.iss71402.supportGFSv16
-export HOMEobsproc_prep=/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/gfsv16b/obsproc_prep.iss70457.netcdfhistory
+export HOMEgfs=/gpfs/dell2/emc/modeling/noscrub/Lin.Gan/ecflow-dell/ecf_gfsv16_low
+export HOMEobsproc_global=/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/gfsv16b/obsproc_global_RB-3.4.0
+export HOMEobsproc_network=/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/gfsv16b/obsproc_global_RB-3.4.0
+export HOMEobsproc_prep=/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/obsproc/gfsv16b/obsproc_prep_RB-5.4.0
